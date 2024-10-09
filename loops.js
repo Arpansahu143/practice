2035bf54f3b5942fa3d17755a9c1a7d3721cc76f
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-10-09 10:02:15 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,24 @@
+let obj={
+    name:"ARPAN",
+    job:"student",
+    age:"19"
+}
+
+for(const key in obj){
+    console.log(key);
+}
+
+for(const c of "ARPAN"){
+    console.log(c)
+}
+
+i=0;
+while(i<6){
+    console.log(i)
+    i++;
+}
+k=10
+do{
+    console.log(k)
+    k++;
+}while(k<3);
