c9517ef9a5971043dab3035a214864ed3ec0f96b
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-10-09 10:02:09 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,36 @@
+console.log("ok bro");
+
+console.log("--------------------------------");
+
+function nice(name) {
+  console.log("hey " + name + " you are nice!");
+}
+
+nice("ARPAN");
+nice("sivam");
+
+console.log("--------------------------------");
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(100, 200);
+
+console.log("--------------------------------");
+
+function mul1(c, d) {
+  return c * d;
+}
+result = mul1(2, 10);
+console.log("the multiplication is ", result);
+
+console.log("--------------------------------");
+
+// default parameter
+
+function mul(o, j,k=2) {
+    return o * j*k;
+  }
+  result1 = mul(5, 1);
+  console.log("the multiplication is ", result1);
