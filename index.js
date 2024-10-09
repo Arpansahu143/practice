cecad7981ffea4a51ad2b02346ab7d48bdb6b4bc
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-10-09 10:02:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello i'm arpan sahu");
+let age=17;
+let grace=2;
+console.log(age+grace)
+if((age+grace)>=18){
+    console.log("you can drive");
+}
+else{
+    console.log("you cannot drive");
+}
+console.log("age ka power grace", age**grace)
+console.log("age + grace", age+grace)
