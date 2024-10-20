cce6957915c78c280c4e71a54eb60f3e43872fa5
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-10-20 12:14:01 +0530

    Create v60.js

---
+++ v60.js
@@ -0,0 +1,26 @@
+console.log("hello sir")
+let a="arpan"
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+// template literals
+
+let name="danger"
+let friend="dynamo"
+console.log(`his name is ${name} and his friend name is ${friend}`)
+
+// -----------------
+
+
+for(let i=0;i<5;i++){
+    console.log("ok")
+}
+
+// ------------------------
+
